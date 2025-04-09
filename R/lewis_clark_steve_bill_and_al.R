b2b4ecefb093111e7559df73f36a06ef0166a314
Tim Bender <bendertj@gmail.com>
2025-04-09 11:35:46 -0400

    renv

--- R/lewis_clark_steve_bill_and_al.R
+++ R/lewis_clark_steve_bill_and_al.R
@@ -1,6 +1,15 @@
 renv::use(
-  renv = "renv@1.1.1"
+  cpp11      = "cpp11@0.5.1",
+  generics   = "generics@0.1.3",
+  lubridate  = "lubridate@1.9.4",
+  renv       = "renv@1.1.1",
+  timechange = "timechange@0.3.0"
 )
 
 
 library(renv)
+library(lubridate)
+library(ggplot2)
+
+
+renv::snapshot()
